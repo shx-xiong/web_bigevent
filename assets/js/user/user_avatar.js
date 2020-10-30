4cd140067681faf49feae4b2954c9e5dc772db8d
shx-xiong <shuxiang_x@163com>
2020-10-30 10:34:52 +0800

    完成个人中心功能

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -11,4 +11,47 @@ $(function () {
 
     // 1.3 创建裁剪区域
     $image.cropper(options)
+
+    //点击按钮选择图片
+    $('#btnChooseImage').on('click', function () {
+        $('#file').click()
+    })
+
+    //更换裁剪区域图片
+    $('#file').on('change', function (e) {
+        var file = this.files[0]
+        if (file === undefined) {
+            return layer.msg('请选择图片!')
+        }
+        var newImgURL = URL.createObjectURL(file)
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', newImgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    })
+
+    //上传图片
+    $('#btnUpload').on('click', function () {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        $.ajax({
+            method: 'post',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg(res.message)
+                window.parent.getUserInfo()
+            }
+        })
+
+    })
 })
